58415a8c02e482c1c55ae03cd9d1cebc82874e2a
Gaurav Sisodiya <gauravsisodiya8532@gmail.com>
2024-12-03 17:17:03 +0530

    'icon-Logic-par-kaam-kiya'

--- 01-Accordion/script.js
+++ 01-Accordion/script.js
@@ -1,32 +1,35 @@
 const faqQuestion = document.querySelectorAll('.faqQuest')
 const icon = document.querySelectorAll('.fa-solid')
 
-faqQuestion.forEach((items,index)=>{
-    items.addEventListener('click',()=>{
+faqQuestion.forEach((items, index) => {
+    items.addEventListener('click', () => {
         let currectAns = items.nextElementSibling;
-        let iconChange = items.lastChild.previousSibling;
         // console.log(items.lastChild)
         // console.log(items.lastChild.previousSibling)
 
         let allFaqAns = document.querySelectorAll('.faqAns')
-        allFaqAns.forEach((itm,i)=>{
-            if(allFaqAns[i] !== currectAns){
+        allFaqAns.forEach((itm, i) => {
+            if (allFaqAns[i] !== currectAns) {
                 itm.classList.remove('active')
             }
         })
 
-        icon.forEach((allIcon,allIndex)=>{
-            if(icon[allIndex] !== iconChange){
+        let iconChange = items.lastChild.previousSibling;
+        icon.forEach((allIcon, allIndex) => {
+            if (allIcon != iconChange) {
                 allIcon.classList.remove('fa-minus')
                 allIcon.classList.add('fa-plus')
                 // console.log(icon[allIndex])
                 // console.log(iconChange)
+            } else {
+                iconChange.classList.toggle('fa-minus')
+                iconChange.classList.toggle('fa-plus')
             }
         })
 
         currectAns.classList.toggle('active')
-        iconChange.classList.toggle('fa-minus')
-        iconChange.classList.toggle('fa-plus')
-        
+        // iconChange.classList.toggle('fa-minus')
+        // iconChange.classList.toggle('fa-plus')
+
     })
 })
